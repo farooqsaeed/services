f5e0b8b04038433e937366d462bf35f595743570
Farooq Saeed <60470178+farooqsaeed@users.noreply.github.com>
2022-06-13 11:37:39 +0500

    settings changes

---
+++ app/Models/landloard_auto_forwarding.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class landloard_auto_forwarding extends Model
+{
+    use HasFactory;
+}
---
+++ database/migrations/2022_06_13_062908_create_settings_table.php
@@ -0,0 +1,43 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('settings', function (Blueprint $table) {
+            $table->id();
+            $table->integer('user_id');
+            $table->string('welcome_message');
+            $table->string('level_one');
+            $table->string('description_one');
+            $table->string('level_second');
+            $table->string('description_second');
+            $table->string('level_third');
+            $table->string('description_third');
+            $table->string('opening_hour');
+            $table->string('phone');
+            $table->string('email_description');
+            $table->string('app_message');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('settings');
+    }
+};
