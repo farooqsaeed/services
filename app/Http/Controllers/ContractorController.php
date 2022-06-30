fa1943fcf0c043879f091f4cb7731a73e2409b64
Farooq Saeed <60470178+farooqsaeed@users.noreply.github.com>
2022-06-30 12:49:01 +0500

    lastest

--- app/Http/Controllers/ContractorController.php
+++ app/Http/Controllers/ContractorController.php
@@ -28,7 +28,8 @@ class ContractorController extends Controller
      */
     public function create()
     {
-        $groups = Group::orderby('id', 'DESC')->get();
+        $groups = Group::orderby('id', 'DESC')
+        ->get();
         return view('contractors.add', compact(['groups']));
     }
     /**
